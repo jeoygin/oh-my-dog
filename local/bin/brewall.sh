e24556c74c905c190ae89104477bf830b554e289
Jeoygin <jeoygin@gmail.com>
2017-10-14 21:18:20 -0700

    Add brewall.sh

---
+++ local/bin/brewall.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# A code-searching tool similar to ack, but faster.
+# https://github.com/ggreer/the_silver_searcher
+brew install the_silver_searcher 
+
+# A simple, fast and user-friendly alternative to find.
+# https://github.com/sharkdp/fd
+brew install fd
+
+# Send User Notifications on macOS from the command-line.
+# https://github.com/julienXX/terminal-notifier
+brew install terminal-notifier
+
+# Record and share your terminal sessions, the right way
+# https://asciinema.org/
+brew install asciinema
