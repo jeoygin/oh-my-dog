e92eedb60696125a6a2a5b0b52b8ce1ab6f76f67
Jeoygin Wang <jeoygin@gmail.com>
2018-02-16 10:02:14 -0800

    Update statup.sh to restore tmux sessions

--- local/bin/startup.sh
+++ local/bin/startup.sh
@@ -3,3 +3,9 @@
 ssh-add -K
 gpg-agent --daemon
 emacs -nw --no-desktop --daemon=ws
+
+tmux.sh new-session -s zombie -d
+tmux.sh send-keys -t zombie ~/.tmux/plugins/tmux-resurrect/scripts/restore.sh C-m
+sleep 3
+tmux.sh kill-session -t zombie
+tmux.sh a
