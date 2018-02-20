ecfb5c3c6c473658067c4bd036b9ce5e9f3cce65
Jeoygin Wang <jeoygin@gmail.com>
2018-02-19 16:57:53 -0800

    Update startup.sh

--- local/bin/startup.sh
+++ local/bin/startup.sh
@@ -5,7 +5,8 @@ gpg-agent --daemon
 emacs -nw --no-desktop --daemon=ws
 
 tmux.sh new-session -s zombie -d
-tmux.sh send-keys -t zombie ~/.tmux/plugins/tmux-resurrect/scripts/restore.sh C-m
 sleep 3
+tmux.sh send-keys -t zombie ~/.tmux/plugins/tmux-resurrect/scripts/restore.sh C-m
+sleep 5
 tmux.sh kill-session -t zombie
 tmux.sh a
