396a5db053fec7ba28cae289a645a2b9373e1ae1
Jeoygin Wang <jeoygin@gmail.com>
2018-04-06 10:08:37 -0700

    Update pipall.sh

--- local/bin/pipall.sh
+++ local/bin/pipall.sh
@@ -1,3 +1,6 @@
 # percol adds flavor of interactive selection to the traditional pipe concept on UNIX.
 pip install percol
-
+# Urlscan is a small program that is designed to integrate with the "mutt" mailreader to
+# allow you to easily launch a Web browser for URLs contained in email messages. It is a
+# replacement for the "urlview" program.
+pip install urlscan
