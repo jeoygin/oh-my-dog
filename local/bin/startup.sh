b91446541b6d8bcdb15bb8e1925dd3073850a2e6
Jeoygin Wang <jeoygin@gmail.com>
2018-04-30 16:51:23 -0700

    Update startup scrip

--- local/bin/startup.sh
+++ local/bin/startup.sh
@@ -10,8 +10,10 @@ while [ "$( tmux.sh list-sessions 2>/dev/null | grep zombie | wc -l )" -eq 0 ]; 
   sleep 1
 done
 tmux.sh send-keys -t zombie ~/.tmux/plugins/tmux-resurrect/scripts/restore.sh C-m
-while [ "$( tmux.sh list-sessions 2>/dev/null | wc -l )" -lt 2 ]; do
+retries=0
+while [ "$( tmux.sh list-sessions 2>/dev/null | wc -l )" -lt 2 ] && [ $retries -lt 5 ]; do
   sleep 1
+  retries=$(( retries+1 ))
 done
 tmux.sh kill-session -t zombie
 tmux.sh a
