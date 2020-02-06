3c72cf35559cbaefc477c43b4f822d57eda2855b
Jeoygin Wang <jeoygin@gmail.com>
2020-02-05 17:11:28 -0800

    Update tmux conf and startup.sh

--- local/bin/startup.sh
+++ local/bin/startup.sh
@@ -2,6 +2,7 @@
 
 ssh-add -K
 gpg-agent --daemon
+sed -i '/smart-mode-line-respectful/d' ~/.emacs.d/custom.el
 emacs -nw --no-desktop --daemon=ws
 
 tmux.sh new-session -s zombie -d
@@ -10,10 +11,10 @@ while [ "$( tmux.sh list-sessions 2>/dev/null | grep zombie | wc -l )" -eq 0 ]; 
   sleep 1
 done
 tmux.sh send-keys -t zombie ~/.tmux/plugins/tmux-resurrect/scripts/restore.sh C-m
-retries=0
-while [ "$( tmux.sh list-sessions 2>/dev/null | wc -l )" -lt 2 ] && [ $retries -lt 5 ]; do
-  sleep 1
-  retries=$(( retries+1 ))
-done
-tmux.sh kill-session -t zombie
+#retries=0
+#while [ "$( tmux.sh list-sessions 2>/dev/null | wc -l )" -lt 2 ] && [ $retries -lt 5 ]; do
+#  sleep 1
+#  retries=$(( retries+1 ))
+#done
+#tmux.sh kill-session -t zombie
 tmux.sh a
